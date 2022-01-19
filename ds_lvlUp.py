b4cbd5c8e60ee3b1c7a2fd64ede43df2bb8bcba6
dEnysss <58307006+MsLolita@users.noreply.github.com>
2022-01-19 17:42:10 +0200

    Update ds_lvlUp.py

--- ds_lvlUp.py
+++ ds_lvlUp.py
@@ -109,47 +109,11 @@ if __name__ == '__main__':
   
   messages = make_list_from_file(worker_file)
   
-  main = DiscordLevelUp("NDk1MzA0ODYzNTc2NzUyMTM0.YeLk0Q.CzshG7cJY7YzqdMYzgOPfRYh3vw", messages, worker_file)
+  main = DiscordLevelUp("ODk4MTk2ODQ2NzkzNjEzMzUz.YeSD-Q.KNiOUUUSx4MwMk1h34syctrEdKs", messages, worker_file)
 
-  main.add_server("930102591923294228", 122)
-  main.add_server("931600696594284654", 122)
-  #main.add_server("929427655462379620", 62)
-  #main.add_server("928072746083188847", 54)  # chine 928072746083188847
-  # main.add_server("928072819881955359", 34)  # japan 928072819881955359
-  # main.add_server("928072857580347493", 34)  # korean 928072857580347493
-  # main.add_server("929096939230879824", 34)  # hindi 929096939230879824
-  # main.add_server("928516544621867058", 34)  # dutch 928516544621867058
-  # main.add_server("929109408351395920", 34)  # french 929109408351395920
-  # main.add_server("928072904497852446", 34)  # spanish 928072904497852446
- # main.add_server("928073050107314196", 34)  # italian 928073050107314196
-  # main.add_server("928073004322283550", 34)  # german 928073004322283550
-  # main.add_server("928072781973831701", 34)  # indonesian 928072781973831701
-  # main.add_server("928516201917841409", 34)  # philliphine 928516201917841409
-    
-    
-  # main.add_server("926068232354414633", 35) 
+  main.add_server("929436445159096391", 303)
+  main.add_server("929436444475392047", 62)
   main.sendMessages()
 
-  
 
-  # berok = DiscordLevelUp("ODk1MTY2Nzc0ODM4OTYwMTY4.YWrvQg.FW8wRvymfoAE1ugAPQwMtC3uMrE", messages, worker_file)
-
-  # berok.add_server("929363885872521306", 63)
-  #berok.add_server("929363885671215152", 42)# turkich 929363885671215152
-  #berok.add_server("929384159552626759", 42)# dutch 929384159552626759
-  #berok.add_server("929363885469876237", 62)# general 929363885469876237
-  #berok.add_server("929391884919525377", 28)# indian 929391884919525377
-    
-    
-    
-  # berok.sendMessages()
-  # lechko = DiscordLevelUp("ODkwMjM3NDc5ODkyOTYzMzI4.YVyGOA.VdJvfTC6N5s0rjUZ_2KV6z4nhMk")
-  # lechko.add_server("885436021125312532", 13)
-  # lechko_thread = threading.Thread(target=lechko.sendMessages)
-  # lechko_thread.start()
-# oblik NDk1MzA0ODYzNTc2NzUyMTM0.YViPTg.jWHVmxkeNMe8sz9fPo6lrghsdPU
-# mofarok 1profile adspower ODk4MTkzMzk0OTE0OTgzOTY3.YWgp9g.SFe6G70c-Z6zjv_mWVAG1xEqTds
-# profile_3_cofob ODk4MTk3NjEyNTUxODY4NDE2.YWgt5g.73pIJVwYpQ_fGW13gy7ZxxY_ChU
-
-
-time.sleep(100000)
+time.sleep(1000000)
